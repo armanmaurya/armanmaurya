ba04bc1648c57df376368fa614b5b1753ef13dc1
Arman Maurya <armanmarya6@gmail.com>
2025-09-29 11:52:16 +0530

    fix: export basepath

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? '/armanmaurya' : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
